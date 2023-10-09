bc6419305f3566313ae1e189a169db7cdbfe7548
amalmechergui <amal123mechergui@gmail.com>
2023-10-10 00:02:22 +0100

    exercice_js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//exercice 1//
 
 let num1 =2;
 let num2 =5;
@@ -5,5 +6,19 @@ let test1 =num1%2;
 let test2 = num2%2;
 let result1=test1==0;
 let result2=test2==0;
-console.log(`"Is"${num1}  an even number? ${result1}`);
-console.log(`"Is"${num2}  an even number?${result2}`);
+console.log(`"Is"${num1} an even number? ${result1}`);
+console.log(`"Is"${num2} an even number?${result2}`);
+
+
+// exercice 2//
+let now ="Now in";
+let three =3;
+let d ="D!";
+console.log(`${now} ${three}${d} `)
+
+//exercice 3//
+
+let country =0;
+country+=5;
+country+=3;
+console.log(`${country}`)
